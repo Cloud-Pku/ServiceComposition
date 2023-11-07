31379fc71d2b7e6b533ff7d2c980001579b377b1
陈云 <PJLAB\chenyun@pjnl104220140l.pjlab.org>
2023-11-07 12:41:07 +0800

    add dqn baseline

--- pipeline_dqn.py
+++ pipeline_dqn.py
@@ -13,6 +13,7 @@ random.seed(ss)
 np.random.seed(ss)
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+print(device)
 max_node_num = 10
 max_ser_set = 10
 bs = 64
@@ -25,7 +26,7 @@ mean_reward_his = []
 loss_his = []
 
 if __name__ == '__main__':
-    writer = SummaryWriter(log_dir=f'/home/PJLAB/chenyun/ServiceComposition/debug/node{max_node_num}-serset{max_ser_set}/{model}', filename_suffix='empty')
+    writer = SummaryWriter(log_dir=f'/home/PJLAB/chenyun/ServiceComposition/dqn/node{max_node_num}-serset{max_ser_set}/{model}', filename_suffix='empty')
     env = ServiceComEnv(max_node_num, max_ser_set)
     if model == 'MHA':
         agent = MHADecision(max_node_num, max_ser_set).to(device)
@@ -46,14 +47,21 @@ if __name__ == '__main__':
         ser_idx, ser_prob = agent.forward(tasks, constraints, masks, topologicals)
         rewards = env.step(ser_idx.numpy())
         rewards = torch.from_numpy(rewards)
+        if model == 'DQN':
+            agent.reward_list.append(rewards)
+            agent.post_process()
+            loss = agent.learn(optim, writer, epoch)
+            writer.add_scalar(f'total_loss', loss, epoch)
+        else:
+            loss = -torch.mean(torch.sum(torch.log(ser_prob), 1) * rewards.squeeze(-1))
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            loss = loss.mean().cpu().item()
+            writer.add_scalar(f'loss', loss, epoch)
         # mean_reward_his.append(rewards.mean().cpu().item())
         writer.add_scalar(f'reward', rewards.mean().cpu().item(), epoch)
-        loss = -torch.mean(torch.sum(torch.log(ser_prob), 1) * rewards.squeeze(-1))
-        # loss_his.append(loss.mean().cpu().item())
-        writer.add_scalar(f'loss', loss.mean().cpu().item(), epoch)
-        optim.zero_grad()
-        loss.backward()
-        optim.step()
+
     
     # plt.plot(range(epoch_size), mean_reward_his, label='reward')
     # plt.savefig('/home/PJLAB/chenyun/ServiceComposition/reward.png')
--- solver/model.py
+++ solver/model.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+import numpy as np
+from torch.utils.data import Dataset
+import random
 
 class NaiveMLPDecision(nn.Module):
     def __init__(self, max_node_num, max_ser_set, att_num=4, hidden_list=[128, 128, 64]):
@@ -209,7 +212,7 @@ class MatchingDecision(nn.Module):
         qos_aggregation = torch.zeros((query_num, self.max_node_num, self.att_num))
         qos_aggregation[:, :, 0] = -3.
         qos_aggregation[:, :, 1] = 1.
-        qos_aggregation[:, :, 0] = 3.
+        qos_aggregation[:, :, 2] = 3.
         qos_aggregation[:, :, 3] = 1.
         
         availability = torch.ones(query_num)
@@ -219,13 +222,15 @@ class MatchingDecision(nn.Module):
         return_idx = torch.zeros((query_num, self.max_node_num))
         return_prob = torch.zeros((query_num, self.max_node_num))
         
-        for node_idx in reversed(range(self.max_node_num)):
+        for i, node_idx in enumerate(reversed(range(self.max_node_num))):
             topological = topologicals[:, node_idx]
             topological_idx = topological.unsqueeze(-1).unsqueeze(-1).repeat([1, 1, self.max_node_num + self.max_ser_set * 4])
             task = torch.gather(tasks, 1, topological_idx).squeeze(1)
             workflow = task[:, :self.max_node_num]
             response_time = workflow * qos_aggregation[:,:,0]
             response_time = torch.max(response_time, 1)[0]
+            if i == (self.max_node_num - 1):
+                response_time = -3.
             
             qos_agg_feature = torch.cat((response_time.unsqueeze(-1), availability.unsqueeze(-1), throughput.unsqueeze(-1), reliability.unsqueeze(-1)), -1)
             topological_idx_tmp = topological.unsqueeze(-1).unsqueeze(-1).repeat([1, 1, self.emb_dim])
@@ -273,17 +278,278 @@ class MatchingDecision(nn.Module):
         
         return return_idx.to(torch.int16), return_prob
 
+class MemoryReplay(object):
+
+    def __init__(self,
+                 max_size=10000,
+                 bs=64,
+                 obs_size=84,
+                 num_heads = 10,
+                 p = 0.5):
+
+        self.obs = np.zeros((max_size, obs_size), dtype=np.float32)
+        self.reward = np.zeros(max_size, dtype=np.float32)
+        self.action = np.zeros(max_size, dtype=np.int32)
+        self.next_obs = np.zeros((max_size, obs_size), dtype=np.float32)
+        self.weight = np.zeros(max_size, dtype=np.float32)
+        #self.ss = np.zeros_like(self.s)
+        self.done = np.array([True]*max_size)
+        self.head_mask = np.zeros((max_size, num_heads), dtype=np.int32)
+        
+        self.num_heads = num_heads
+        self.p = p
+        self.max_size = max_size
+        self.bs = bs
+        self._cursor = 0
+#        self.total_idx = list(range(self.max_size))
+        self.occupied = False
+
+
+    def put(self, s, a, r, s_, d, w):
+
+        if self._cursor == (self.max_size-1):
+            self._cursor = 0
+            self.occupied = True
+        else:
+            self._cursor += 1
+
+        self.obs[self._cursor] = np.array(s)
+        self.action[self._cursor] = np.array(a)
+        self.reward[self._cursor] = np.array(r)
+        self.next_obs[self._cursor] = np.array(s_)
+        self.done[self._cursor] = np.array(d)
+        self.weight[self._cursor] = np.array(w)
+        self.head_mask[self._cursor] = np.random.binomial(1, self.p, self.num_heads)
+
+
+    def batch(self, k):
+        if self.occupied:
+            idx =  np.array(range(self.max_size))
+            idx_k = self.head_mask[:,k].astype(bool)
+        else:
+            idx =  np.array(range(self._cursor))
+            idx_k = self.head_mask[:self._cursor,k].astype(bool)
+        sample_idx = random.sample(list(idx[idx_k]), self.bs)
+        s = self.obs[sample_idx]
+        a = self.action[sample_idx]
+        r = self.reward[sample_idx]
+        s_ = self.next_obs[sample_idx]
+        d = self.done[sample_idx]
+
+        return torch.from_numpy(s), torch.from_numpy(a), torch.from_numpy(r), torch.from_numpy(s_), torch.from_numpy(d)
+
+# class Buffer(Dataset):
+#     def __init__(self, obs, action, next_obs, reward, done, mask, weight, transition_size) -> None:
+#         super().__init__()
+#         # transition is passed as list and updated outside
+#         self.obs = obs
+#         self.action = action
+#         self.next_obs = next_obs
+#         self.reward = reward
+#         self.done = done
+#         self.mask = mask
+#         self.weight = weight
+#         self.transition_size = transition_size
+#     def __len__(self):
+#         return max(1, len(self.obs))
+#     def __getitem__(self, idx):
+#         idx = idx % len(self.obs)
+#         obs = self.obs[idx]
+#         action = self.action[idx]
+#         next_obs = self.next_obs[idx]
+#         done = self.done[idx]
+#         reward = torch.Tensor([self.reward[idx]])
+#         mask = self.mask[idx]
+#         weight = self.weight[idx]
+#         return (obs, action, next_obs, reward, done, mask, weight)
+
+class DQNDecision(nn.Module):
+    def __init__(self, max_node_num, max_ser_set, att_num=4, emb_dim = 128, q_heads_num = 4, iter_num = 10, gamma = 0.9, bias = True):
+        super().__init__()
+        
+        self.iter_step = 0
+        self.target_update_freq = 100
+        self.max_node_num = max_node_num
+        self.max_ser_set = max_ser_set
+        self.emb_dim = emb_dim
+        self.att_num = att_num
+        self.iter_num = iter_num
+        self.gamma = gamma
+        self.mse = nn.MSELoss()
+        self.state_dim = self.max_node_num + self.max_ser_set * att_num + 2 * att_num
+        self.state_emb_layer = nn.Sequential(
+            nn.Linear(self.state_dim, 128),
+            nn.SiLU(),
+            nn.Linear(128, emb_dim),
+        )
+        self.state_emb_layer_target = nn.Sequential(
+            nn.Linear(self.state_dim, 128),
+            nn.SiLU(),
+            nn.Linear(128, emb_dim),
+        )
+        self.q_heads_num = q_heads_num
+        self.q_heads = nn.ModuleList(
+            [nn.Sequential(
+                nn.SiLU(),
+                nn.Linear(emb_dim, 128),
+                nn.SiLU(),
+                nn.Linear(128, self.max_ser_set),
+            ) for _ in range(q_heads_num)]
+        )
+        self.q_heads_target = nn.ModuleList(
+            [nn.Sequential(
+                nn.SiLU(),
+                nn.Linear(emb_dim, 128),
+                nn.SiLU(),
+                nn.Linear(128, self.max_ser_set),
+            ) for _ in range(q_heads_num)]
+        )
+        self.obs_list = []
+        self.action_list = []
+        self.next_obs_list = []
+        self.reward_list = []
+        self.done_list = []
+        self.mask_list = []
+        self.weights_list = []
+        self.buffer = MemoryReplay(obs_size=self.state_dim, num_heads=q_heads_num)
+        
+    def make_onehot(self, topological):
+        ones_matrix = torch.zeros((topological.shape[0], self.max_node_num))
+        ones_matrix[torch.arange(topological.shape[0]), topological] = 1
+        return ones_matrix
+
+    def forward(self, tasks, constraints, masks, topologicals):
+        # constraints = constraints.unsqueeze(1).repeat([1, self.max_node_num, 1]) # just cat
+        
+        k = np.random.randint(0, self.q_heads_num)
+        workflows = tasks[:,:, :self.max_node_num]
+        
+        query_num = tasks.shape[0]
+        qos_aggregation = torch.zeros((query_num, self.max_node_num, self.att_num))
+        qos_aggregation[:, :, 0] = -3.
+        qos_aggregation[:, :, 1] = 1.
+        qos_aggregation[:, :, 2] = 3.
+        qos_aggregation[:, :, 3] = 1.
+        
+        availability = torch.ones(query_num)
+        throughput = torch.zeros(query_num) + 3
+        reliability = torch.ones(query_num)
+        
+        return_idx = torch.zeros((query_num, self.max_node_num))
+        # return_prob = torch.zeros((query_num, self.max_node_num))
+        
+        for i, node_idx in enumerate(reversed(range(self.max_node_num))):
+            topological = topologicals[:, node_idx]
+            topological_idx = topological.unsqueeze(-1).unsqueeze(-1).repeat([1, 1, self.max_node_num + self.max_ser_set * 4])
+            task = torch.gather(tasks, 1, topological_idx).squeeze(1)
+            workflow = task[:, :self.max_node_num]
+            response_time = workflow * qos_aggregation[:,:,0]
+            response_time = torch.max(response_time, 1)[0]
+            if i == 0:
+                response_time -= 3.
+            qos_agg_feature = torch.cat((response_time.unsqueeze(-1), availability.unsqueeze(-1), throughput.unsqueeze(-1), reliability.unsqueeze(-1)), -1)
+            # topological_idx_tmp = topological.unsqueeze(-1).unsqueeze(-1).repeat([1, 1, task])
+            state = torch.cat((task, constraints, qos_agg_feature), -1) # maybe FiLM
+            self.obs_list.append(state)
+            if i != 0:
+                self.next_obs_list.append(state)
+                self.reward_list.append(torch.zeros(query_num))
+                self.done_list.append(torch.zeros(query_num))
+            self.mask_list.append(torch.randint(0, 2, (query_num, self.q_heads_num)))
+            state_emb = self.state_emb_layer(state)
+
+            q_value = self.q_heads[k](state_emb)
+            
+            topological_idx_tmp = topological.unsqueeze(-1).unsqueeze(-1).repeat([1, 1, self.max_ser_set])
+            mask = torch.gather(masks, 1, topological_idx_tmp).squeeze(1)
+            q_value = q_value.masked_fill(mask == 0, -1e9)
+            ser_idx = torch.argmax(q_value, -1)
+            self.action_list.append(ser_idx)
+            
+            q = q_value.gather(1, ser_idx.unsqueeze(-1))
+            q_ = self.q_heads_target[k](self.state_emb_layer_target(state)).gather(1, ser_idx.unsqueeze(-1))
+            self.weights_list.append(torch.abs(q - q_).detach())
+            
+            topo_onehot = self.make_onehot(topological)
+            idx_resi_mtx = topo_onehot * ser_idx.unsqueeze(-1)
+            return_idx += idx_resi_mtx
+            
+            
+            # calculate aggretation
+            rt_idx = ser_idx * 4 + self.max_node_num
+            ava_idx = ser_idx * 4 + self.max_node_num + 1
+            tp_idx = ser_idx * 4 + self.max_node_num + 2
+            rel_idx = ser_idx * 4 + self.max_node_num + 3
+            
+            ser_rt = task.gather(1, rt_idx.unsqueeze(-1)).squeeze(-1)
+            ser_ava = task.gather(1, ava_idx.unsqueeze(-1)).squeeze(-1)
+            ser_tp = task.gather(1, tp_idx.unsqueeze(-1)).squeeze(-1)
+            ser_rel = task.gather(1, rel_idx.unsqueeze(-1)).squeeze(-1)
+            qos_aggregation[torch.arange(query_num), topological, 0] = ser_rt + response_time
+            qos_aggregation[torch.arange(query_num), topological, 1] = ser_ava * availability
+            qos_aggregation[torch.arange(query_num), topological, 2] = torch.min(torch.cat((ser_tp.unsqueeze(-1), throughput.unsqueeze(-1)), -1), 1)[0]
+            qos_aggregation[torch.arange(query_num), topological, 3] = ser_rel * reliability
+            availability = ser_ava * availability
+            throughput = torch.min(torch.cat((ser_tp.unsqueeze(-1), throughput.unsqueeze(-1)), -1), -1)[0]
+            reliability = ser_rel * reliability
+        
+        self.next_obs_list.append(state)
+        self.done_list.append(torch.ones(query_num))
+
+        return return_idx.to(torch.int16), None
+    
+    def post_process(self):
+        task_num = len(self.obs_list)
+        query_num = self.obs_list[0].shape[0]
+        for task in range(task_num):
+            for query in range(query_num):
+                s = self.obs_list[task][query]
+                a = self.action_list[task][query]
+                r = self.reward_list[task][query]
+                s_ = self.next_obs_list[task][query]
+                d = self.done_list[task][query]
+                w = self.weights_list[task][query]
+                self.buffer.put(s, a, r, s_, d, w)
+        self.obs_list = []
+        self.action_list = []
+        self.reward_list = []
+        self.next_obs_list = []
+        self.done_list = []
+        self.weights_list = []
+
+    def learn(self, optim, writer, epoch):
+        total_loss_his = []
+        for iter in range(self.iter_num):
+            loss_his = []
+            for k in range(self.q_heads_num):
+                k = np.random.randint(0, self.q_heads_num)
+                s, a, r, s_, d = self.buffer.batch(k)
+                q = self.q_heads[k](self.state_emb_layer(s)).gather(1, a.unsqueeze(-1).to(torch.int64)).squeeze(-1)
+                y = r + self.gamma * (~d) * torch.max(self.q_heads_target[k](self.state_emb_layer_target(s_)), -1)[0]
+                q_loss = self.mse(q, y.detach())
+                optim.zero_grad()
+                q_loss.backward()
+                optim.step()
+                loss_his.append(q_loss.detach().cpu().item())
+            writer.add_scalars(f'Loss/q_{k}_loss', np.mean(loss_his), epoch)
+            total_loss_his.append(np.mean(loss_his))
+        return np.mean(total_loss_his)
+
+        
 if __name__ == '__main__':
     max_node_num = 3
     max_ser_set = 4
-    agent = MatchingDecision(max_node_num, max_ser_set)
-    tasks = torch.randint(0, 2, (64, max_node_num, max_node_num + max_ser_set * 4))
-    constraints = torch.rand((64, 4))
-    masks = torch.randint(0, 2, (64, max_node_num, max_ser_set))
-    topologicals = torch.arange(0, max_node_num).unsqueeze(0).repeat([64, 1])
+    # agent = MatchingDecision(max_node_num, max_ser_set)
+    # tasks = torch.randint(0, 2, (64, max_node_num, max_node_num + max_ser_set * 4))
+    # constraints = torch.rand((64, 4))
+    # masks = torch.randint(0, 2, (64, max_node_num, max_ser_set))
+    # topologicals = torch.arange(0, max_node_num).unsqueeze(0).repeat([64, 1])
     
+    # res = agent(tasks, constraints, masks, topologicals)
     
-    res = agent(tasks, constraints, masks, topologicals)
+    agent = DQNDecision(max_node_num, max_ser_set)
+    inp = torch.rand((64, max_node_num + 2 * 4))
+    agent(inp)
         
         
         
